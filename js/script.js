179f7d3af5bac583916950f6d2dfa2ef49109dbe
Alex <ammorishita@gmail.com>
2017-04-22 22:31:00 -0700

    Car will stop and go on red and green lights.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var vehicleStats = {
+	'speed' : 12
+};
+
 var engine = {
 	init: function() {
 		this.trafficLights();
@@ -10,14 +14,14 @@ var engine = {
 				stopLight.classList.remove('yellow');
 		    	stopLight.classList.add('red');
 		    	greenLight();		
-			},3000)		
+			},4000)		
 		}
 		function yellowLight() {
 			setTimeout(function() {
 				stopLight.classList.remove('green');
 		    	stopLight.classList.add('yellow');
 		    	redLight();		
-			},2000)
+			},8000)
 		}
 		function greenLight() {
 		    var rand = Math.round(Math.random() * (8000 - 500)) + 1500;
@@ -26,7 +30,7 @@ var engine = {
 		    	stopLight.classList.remove('red');
 		    	stopLight.classList.add('green');
 		    	yellowLight();
-		    }, 2000);
+		    }, 8000);
 		};
 		greenLight();
 	},
@@ -45,41 +49,67 @@ var engine = {
 			vehicle.style.top = this.posY;
 			vehicle.style.left = this.posX;
 		};
+		Car.prototype.remove = function() {
+			const currentVehicle = document.querySelector('.vehicle');
+			currentVehicle.parentNode.removeChild(currentVehicle);
+		}
 		Car.prototype.drive = function() {
 			let posY = parseInt(this.posY);
+			let posX = parseInt(this.posX);
 			let driving = true;
 			let stopped = false;
 			const trafficLight = lights.style.top;
+			const greenZone = document.querySelector('.go');
+			const yellowZone = document.querySelector('.brake');
+			const redZone = document.querySelector('.stop');
+			const greenPosY = parseInt(window.getComputedStyle(greenZone, null).getPropertyValue('top'));
+			const yellowPosY = parseInt(window.getComputedStyle(yellowZone, null).getPropertyValue('top'));
+			const redPosY = parseInt(window.getComputedStyle(redZone, null).getPropertyValue('top'));
+			const yellowPosYEnd = yellowPosY + 50;
+			const greenPosYEnd = yellowPosY;
+			const redPosYEnd = redPosY + 30;
 			function slowDown() {
 				let currentY = vehicle.style.top;
+				vehicle.style.top = posY + 'px';
+				//vehicleStats.speed += 2;
 			}
-			function speedUp() {
+			function drive() {
+				vehicle.style.top = posY + 'px';		
+				posY = posY += 1;									
+			}
+			function moving() {
 				let spd = 12;
 				(function loop() {
 				    //var rand = Math.round(Math.random() * (3000 - 500)) + 500;
 				    setTimeout(function() {
+				        loop();
 				    	if(driving) {
-				            drive();
-				            loop(); 
-				            console.log('driving') 
+				    		drive();
 				    	}
-				    	if(lights.classList.contains('yellow')) {
-				    		spd += 2;
-				    		if(spd == 100) {
+				    	if(lights.classList.contains('green')) {
+				    		driving = true;
+				    		const car = new Car();
+				    		vehicleStats.speed = vehicleStats.speed -=2;
+				    		if(vehicleStats.speed < 13) {
+				    			vehicleStats.speed = 12;
+				    		}
+				    	}
+				    	if(lights.classList.contains('red') && posY < yellowPosYEnd && posY > yellowPosY) {
+				    		vehicleStats.speed = vehicleStats.speed += 2;
+				    		if(vehicleStats.speed > 100) {
+				    			vehicleStats.speed = 80;
 				    			driving = false;
 				    		}
 				    	}
-				    	if(posY > 550) {
-							driving = false;
+				    	if(posY > 575) {
+							vehicle.style.top = '0px';
+							posY = 0;
+							vehicle.style.left = (Math.floor(Math.random() * 100 + 80)) +'px';			
 						}
-				    }, spd);
+				    }, vehicleStats.speed);
 				}());
-				function drive() {
-					vehicle.style.top = posY + 'px';		
-					posY = posY += 1;									
-				}
 			};
-			speedUp();
+			moving();
 		}
 		const car = new Car();
 		car.render();
