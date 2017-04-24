3c1017f631116d34f420ed0220b76a84f0692288
Alex <ammorishita@gmail.com>
2017-04-23 18:10:55 -0700

    Create individual car objects and remove them when they reach the end of the road.

--- js/script.js
+++ js/script.js
@@ -36,76 +36,95 @@ var engine = {
 	},
 	vehicles: function() {
 		const road = document.querySelector('.road');
-		const vehicle = document.createElement('div');
 		const lights = document.querySelector('.intersection');
+		const trafficLight = lights.style.top;
+		const greenZone = document.querySelector('.go');
+		const yellowZone = document.querySelector('.brake');
+		const redZone = document.querySelector('.stop');
+		const greenPosY = parseInt(window.getComputedStyle(greenZone, null).getPropertyValue('top'));
+		const yellowPosY = parseInt(window.getComputedStyle(yellowZone, null).getPropertyValue('top'));
+		const redPosY = parseInt(window.getComputedStyle(redZone, null).getPropertyValue('top'));
+		const yellowPosYEnd = yellowPosY + 50;
+		const greenPosYEnd = yellowPosY;
+		const redPosYEnd = redPosY + 30;
+
 		const Car = function() {
-			this.posX = '100px',
-			this.posY = '0px',
-			this.color = 'red'
+			this.element = document.createElement('div')
+			this.element.classList.add('vehicle');
+			this.x = this.element.style.left;
+			this.y = this.element.style.top;
+			this.posX = 100,
+			this.posY = 0,
+			this.className = 'vehicle'
 		};
 		Car.prototype.render = function() {
-			vehicle.classList.add('vehicle');
-			road.appendChild(vehicle);
-			vehicle.style.top = this.posY;
-			vehicle.style.left = this.posX;
+			road.appendChild(this.element);
+			let randomX = Math.floor(Math.random() * 100) + 100;
+			this.element.style.left = randomX + 'px';
+			this.element.style.top = this.posY + 'px';
 		};
 		Car.prototype.remove = function() {
-			const currentVehicle = document.querySelector('.vehicle');
+			let currentVehicle = document.querySelector('.vehicle');
 			currentVehicle.parentNode.removeChild(currentVehicle);
-		}
+		};
 		Car.prototype.drive = function() {
-			let posY = parseInt(this.posY);
-			let posX = parseInt(this.posX);
+			const vehicle = document.querySelector('.vehicle');
+			let self = this;
+			let posY = self.posY;
+			let posX = self.posX;
 			let driving = true;
 			let stopped = false;
-			const trafficLight = lights.style.top;
-			const greenZone = document.querySelector('.go');
-			const yellowZone = document.querySelector('.brake');
-			const redZone = document.querySelector('.stop');
-			const greenPosY = parseInt(window.getComputedStyle(greenZone, null).getPropertyValue('top'));
-			const yellowPosY = parseInt(window.getComputedStyle(yellowZone, null).getPropertyValue('top'));
-			const redPosY = parseInt(window.getComputedStyle(redZone, null).getPropertyValue('top'));
-			const yellowPosYEnd = yellowPosY + 50;
-			const greenPosYEnd = yellowPosY;
-			const redPosYEnd = redPosY + 30;
 			function slowDown() {
 				let currentY = vehicle.style.top;
 				vehicle.style.top = posY + 'px';
 				//vehicleStats.speed += 2;
 			}
 			function drive() {
-				vehicle.style.top = posY + 'px';		
-				posY = posY += 1;									
+				self.element.style.top = self.posY +'px';
+				self.posY = self.posY +=1;					
 			}
 			function moving() {
+				let vehicles = document.querySelectorAll('.vehicle');
 				let spd = 12;
 				(function loop() {
 				    //var rand = Math.round(Math.random() * (3000 - 500)) + 500;
+				    let roll = Math.floor(Math.random() * 10);
 				    setTimeout(function() {
 				        loop();
 				    	if(driving) {
 				    		drive();
 				    	}
+				    	if(self.posY == 180) {
+				    		const car = new Car();
+				    		let roll = Math.random();
+							if(roll > 1) {
+								car.render();
+								car.drive();
+							}
+				    		let vehicles = document.querySelectorAll('.vehicle');
+				    	}
 				    	if(lights.classList.contains('green')) {
 				    		driving = true;
-				    		const car = new Car();
 				    		vehicleStats.speed = vehicleStats.speed -=2;
 				    		if(vehicleStats.speed < 13) {
 				    			vehicleStats.speed = 12;
 				    		}
 				    	}
 				    	if(lights.classList.contains('red') && posY < yellowPosYEnd && posY > yellowPosY) {
+				    		console.log('light is red')
 				    		vehicleStats.speed = vehicleStats.speed += 2;
 				    		if(vehicleStats.speed > 100) {
 				    			vehicleStats.speed = 80;
 				    			driving = false;
 				    		}
 				    	}
-				    	if(posY > 575) {
-							vehicle.style.top = '0px';
-							posY = 0;
-							vehicle.style.left = (Math.floor(Math.random() * 100 + 80)) +'px';			
-						}
+						if(self.posY > 575) {
+			                for (i=0; i<vehicles.length; i++) {
+			                    if(vehicles[i].style.top == '575px') {
+			                        vehicles[i].remove();                 
+			                    }
+			                }
+			            }
 				    }, vehicleStats.speed);
 				}());
 			};
