d432b62d7a272d062fb69774b757b07261a4c2af
Alex <ammorishita@gmail.com>
2017-05-14 14:01:48 -0700

    Reworked entire map. Removed all zones and added intersections.  Road light timing on set intervals.

--- js/script.js
+++ js/script.js
@@ -1,181 +1,207 @@
-var vehicleStats = {
+let vehicleStats = {
 	'speed' : 25
 };
+let intersectionList = [];
 var engine = {
 	init: function() {
 		this.trafficLights();
 		this.vehicles();
+		this.addVehicles();
 	},
 	trafficLights: function() {
-		const stopLight = document.querySelector('.intersection');
-		function redLight() {
-			setTimeout(function() {
-				stopLight.classList.remove('yellow');
-		    	stopLight.classList.add('red');
-		    	greenLight();		
-			},4000)		
-		}
-		function yellowLight() {
-			setTimeout(function() {
-				stopLight.classList.remove('green');
-		    	stopLight.classList.add('yellow');
-		    	redLight();		
-			},8000)
-		}
+		const stopLight = document.querySelectorAll('.intersection');
+		let active = true;
+		stopLight.forEach(function(e) {
+			e.classList.add('red-light-vertical');
+			e.classList.add('green-light-horizontal');
+		});
+		stopLight.forEach(function(e) {
+			if(active) {
+				intersectionList.push(e.getBoundingClientRect());
+				active = false;
+			}
+		});
+		function greenLight2() {
+			let lightTime = [1500,3000,4500];
+		    stopLight.forEach(function(e) {
+		    	let rand = Math.round(Math.random() * (8000 - 500)) + 1500;
+		    	setTimeout(function() {
+		    		e.classList.remove('green-light-vertical');
+		    		e.classList.add('yellow-light-vertical');
+		    		setTimeout(function() {
+			    		rand = Math.round(Math.random() * (8000 - 500)) + 1500;
+			    		e.classList.remove('yellow-light-vertical');
+			    		e.classList.add('red-light-vertical');
+					    setTimeout(function() {
+				    		let rand = Math.round(Math.random() * (8000 - 500)) + 1500;
+				    		e.classList.remove('red-light-horizontal')
+				    		e.classList.add('green-light-horizontal');
+				    		greenLight();
+				    	}, 1500);
+		    		}, 3000);
+		    	}, 4000);
+		    });
+		};
 		function greenLight() {
-		    var rand = Math.round(Math.random() * (8000 - 500)) + 1500;
-		    setTimeout(function() {
-		    	//green();
-		    	stopLight.classList.remove('red');
-		    	stopLight.classList.add('green');
-		    	yellowLight();
-		    }, 8000);
+		    stopLight.forEach(function(e) {
+		    	let rand = Math.round(Math.random() * (8000 - 500)) + 1500;
+		    	setTimeout(function() {
+		    		e.classList.remove('green-light-horizontal');
+		    		e.classList.add('yellow-light-horizontal');
+		    		setTimeout(function() {
+			    		rand = Math.round(Math.random() * (8000 - 500)) + 1500;
+			    		e.classList.remove('yellow-light-horizontal');
+			    		e.classList.add('red-light-horizontal');
+					    setTimeout(function() {
+				    		let rand = Math.round(Math.random() * (8000 - 500)) + 1500;
+				    		e.classList.remove('red-light-vertical')
+				    		e.classList.add('green-light-vertical');
+				    		greenLight2();
+				    	}, 1500);
+		    		}, 3000);
+		    	}, 4000);
+		    });
 		};
 		greenLight();
 	},
+	addVehicles: function() {
+		let interval = 5000;
+		/*let timer = setInterval(function() {
+			engine.vehicles();
+		},interval);*/
+	},
 	vehicles: function() {
 		const road = document.querySelector('.road');
+		const map = document.querySelector('.container');
 		const lights = document.querySelector('.intersection');
 		const trafficLight = lights.style.top;
-		const greenZone = document.querySelector('.go');
-		const yellowZone = document.querySelector('.brake');
-		const redZone = document.querySelector('.stop');
-		const greenPosY = parseInt(window.getComputedStyle(greenZone, null).getPropertyValue('top'));
-		const yellowPosY = parseInt(window.getComputedStyle(yellowZone, null).getPropertyValue('top'));
-		const redPosY = parseInt(window.getComputedStyle(redZone, null).getPropertyValue('top'));
-		const yellowPosYEnd = yellowPosY + 50;
-		const greenPosYEnd = yellowPosY;
-		const redPosYEnd = redPosY + 30;
+		let allVehicles;
 
 		const Car = function() {
-			this.element = document.createElement('div')
+			this.element = document.createElement('div');
 			this.element.classList.add('vehicle');
-			this.element.classList.add('moving');
-			this.x = this.element.style.left;
+			this.x = parseInt(this.element.style.left);
 			this.y = parseInt(this.element.style.top);
 			this.speed = Math.floor(Math.random() * 2) + 1;
-			this.slowing = false;
-			this.driving = false;
+			this.drivingRight = false;
+			this.drivingUp = true;
+			this.drivingDown = false;
+			this.drivingLeft = false;
 			this.stopped = false;
-			this.accelerate = true;
-			this.posX = 100,
-			this.posY = 0,
+			this.accelerateUp = false;
+			this.accelerateDown = false;
+			this.accelerateLeft = false;
+			this.accelerateRight = false;
+			this.slowingLeft = false;
+			this.slowingRight = false;
+			this.slowingUp = false;
+			this.slowingDown = false;
+			this.posX = 250;
+			this.posY = 780;
 			this.accel = 1;
 			this.addCar = function() {
 				allVehicles.push(new Car());
 			}
 		};
 		Car.prototype.update = function() {
-			let currentY = parseInt(this.element.style.top);
-			let speeds = [.8,1,1.25];
-			if(this.posY > 575) {
-				this.posY = 0;
+			let self = this;
+			this.client = this.element.getBoundingClientRect();
+			let distance = document.elementFromPoint(this.posX, this.posY);
+			let currentY = this.client.top;
+			let currentX = this.client.left;
+			let speeds = [1.4,1.8,1.2];
+			if(this.posY < 0) {
+				this.posY = 780;
 			}
-			if(this.driving) {
-				this.posY = this.posY += speeds[this.speed];				
-			} else if (this.accelerate) {
+			if(this.drivingLeft) {this.posX = this.posX -= speeds[this.speed];}
+			if(this.drivingRight) {this.posX = this.posX += speeds[tis.speed];}
+			if(this.drivingDown) {this.posY = this.posY += speeds[this.speed];}
+			if(this.drivingUp) {this.posY = this.posY -= speeds[this.speed];}
+			if(this.accelerate) {
 				this.accel = this.accel +=.005;
-				this.posY = this.posY + Math.log(this.accel);
+				if(this.accelerateDown) {this.posY = this.posY + Math.log(this.accel)};
+				if(this.accelerateUp) {this.posY = this.posY - Math.log(this.accel)};
+				if(this.accelerateLeft) {this.posX = this.posX - Math.log(this.accel)};
+				if(this.accelerateRight) {this.posX = this.posX + Math.log(this.accel)};
 				if(this.accel > 1.85) {
-					this.accelerate = false;
-					this.driving = true;
+					if(this.accelerateDown) {
+						this.drivingDown = true;
+						this.accelerateDown = false;
+					}
+					if(this.accelerateUp) {
+						this.drivingUp = true;
+						this.accelerateUp = false;
+					}
+					if(this.accelerateRight) {
+						this.drivingRight = true;
+						this.accelerateRight = false;
+					}
+					if(this.accelerateLeft) {
+						this.drivingLeft = true;
+						this.accelerateLeft = false;
+					}
 					this.accel = 1;
 				}
-			} else if (this.slowing) {
+			} else if (this.slowingDown) {
 				this.posY = this.posY + 90/(this.posY * (this.posY/180));
+			} else if (this.slowingUp) {
+				this.posY = this.posY - 90/(this.posY * (this.posY/180));
+			} else if (this.slowingRight) {
+				this.posX = this.posX + 90/(this.posX * (this.posX/180));
+			} else if (this.slowingLeft) {
+				this.posX = this.posX - 90/(this.posX * (this.posX/180));
 			} else if (this.stopped) {
 				this.posY = this.posY;
+				this.posX = this.posX;
 			}
+			/*intersectionList.forEach(function(e) {
+				if((currentY < e.bottom) && (currentY > e.top) && (currentX < e.right) && (currentX > (e.left))) {
+					self.drivingUp = false;
+				}
+			})*/
 		};
 		Car.prototype.render = function() {
-			road.appendChild(this.element);
-			let startingX = [65,145,180];
-			let randomX = Math.floor(Math.random() * 3) + 1;
-			console.log(randomX)
-			this.element.style.left = startingX[randomX] + 'px';
+			map.appendChild(this.element);
+			let startingX = [110,145,190,225,270];
+			let randomX = Math.floor(Math.random() * startingX.length);
+			let selectedX = startingX[randomX];
+			this.element.style.left = selectedX + 'px';
+			this.posX = parseInt(this.element.style.left);
 			this.element.style.top = this.posY + 'px';
+			startingX.splice(randomX,1);
 		};
-		Car.prototype.drive = function() {
-			let self = this;
-			let driving = true;
-			let stopped = false;
-			(function loop() {
-			    let vehiclesSlowing = document.querySelectorAll('.slowing');
-			    let vehiclesMoving = document.querySelectorAll('.moving');
-			    setTimeout(function() {
-			        loop();
-			    	let vehicle = document.querySelector('.vehicle');
-			    	let vehicles = document.querySelectorAll('.vehicle');
-			    	let currentY = Math.floor(parseInt(this.posY));
-			    }, vehicleStats.speed);
-			}());
+		allVehicles = [new Car()];
+		lights.addEventListener('click',newcar);
+		function newcar() {
+			allVehicles.push(new Car())
 		};
-		Car.prototype.move = function(elem,style,unit,from,to,time) {
-			let car = document.querySelector("." + this.element.classList[0])
-		    if(elem) return;
-		    var start = new Date().getTime(),
-		        timer = setInterval(function() {
-		            var step = Math.min(1,(new Date().getTime()-start)/time);
-		            car.style[style] = (from+step*(to-from))+unit;
-		            if( step == 1) clearInterval(timer);
-		            self.posY = car.style[style];
-		        },25);
-		    car.style[style] = from+unit;
-		};
-		let allVehicles = [new Car()];
-		render();
 		function render() {
 			allVehicles.forEach(function(car) {
 				car.render();
 			})
 		};
+		render();
 		function updateCars() {
 	        allVehicles.forEach(function(car) {
 	        	car.update();
-	        	if(car.posY > yellowPosY & car.posY < yellowPosYEnd && lights.classList.contains('red')) {
-	        		///Car will slowdown if the light is red and its in the yellow zone.
-	        		car.slowing = true;
-	        		car.driving = false;
-	        	}
-	        	if(car.posY > (yellowPosY - 50) && car.posY < yellowPosYEnd && lights.classList.contains('yellow')) {
-	        		//Car will slow down if the light is yellow and its in yellow zone.s
-	        		car.slowing = true;
-	        		car.driving = false;
-	        	}
-	        	if(car.posY > redPosY && car.posY < redPosYEnd && lights.classList.contains('red')) {
-	        		//Car will stop if its in the red zone and light is red.
-	        		car.slowing = false;
-	        		car.driving = false;
-	        		car.stopped = true;
-	        	}
-	        	if(lights.classList.contains('green') && car.stopped) {
-	        		//Car will accerlate if light turns green and it was stopped.
-	        		car.stopped = false;
-	        		car.accelerate = true;
-			    }
 	        	car.element.style.top = car.posY + 'px';
-	        	if(car.posY < 180 && car.posY > 178) {
-	        		let roll = Math.random();
-	        		if(roll > .75) {
-		        		engine.vehicles();        			
-	        		}
-	        	}
-	        	if(car.posY > 575) {
-	        		//Remove car when it reaches the end of the road.
-	        		let vehicles = document.querySelectorAll('.vehicle');
-        			for (i=0; i<vehicles.length; i++) {
-	                    if(parseInt(vehicles[i].style.top) >= 575) {
-	                        vehicles[i].remove();               
-	                    }
-	                }
+	        	//car.element.style.left = car.posX + 'px';
+        		//Remove car when it reaches the end of the road.
+        		let vehicles = document.querySelectorAll('.vehicle');
+    			for (i=0; i<vehicles.length; i++) {
+                    if(parseInt(vehicles[i].style.top) <= 0 || parseInt(vehicles[i].style.top) > 800 || parseInt(vehicles[i].style.left) <= 0 || parseInt(vehicles[i].style.left) >= 980) {
+                        vehicles[i].remove();               
+                    }
+                }
+	            if(car.posY < 0) {
 	                if(vehicles.length < 2) {
 	                	engine.vehicles();
-	                }
-	        	}
-	        	let id = requestAnimationFrame(updateCars)
+	                }	            	
+	            }
+	        	requestAnimationFrame(updateCars)
 	        });
 	    };
-	    requestAnimationFrame(updateCars)
+	    updateCars();
 	}
-}
+};
 engine.init();
